95359213384065f0373d37b356d506ef6e3b604d
Golfsap <golf.sap@gmail.com>
2023-08-07 11:55:27 +0700

    Add create new grid button with prompt for size

--- script.js
+++ script.js
@@ -1,23 +1,37 @@
+// UI elements
 const container = document.querySelector(".container");
+const btn = document.querySelector(".btn");
 
-const gridSize = 16
-const numOfSquares = gridSize**2;
-const widthOfSquare = 600/gridSize;
+btn.addEventListener('click', () => {
+    let gridSize = prompt("Number of squares per side for new grid?");
+    createGrid(gridSize);
+});
 
-// Create grid
-for (let i=0; i<numOfSquares; i++) {
-    let div = document.createElement("div");
-    div.style.width = widthOfSquare+"px";
-    div.style.height = widthOfSquare+"px";
-    div.classList.add("grid");
-    container.appendChild(div);
+function createGrid(gridSize) {
+    container.replaceChildren(); /* Clear existing grid */
+    const numOfSquares = gridSize**2;
+    const widthOfSquare = 600/gridSize;
+
+    for (let i=0; i<numOfSquares; i++) {
+        let div = document.createElement("div");
+        div.style.width = widthOfSquare+"px";
+        div.style.height = widthOfSquare+"px";
+        div.classList.add("grid");
+        container.appendChild(div);
+    }
+    addHoverColor();
 }
 
-// Add hover effect
+function addHoverColor() {
 const squares = document.querySelectorAll(".grid");
 squares.forEach(square => {
     square.addEventListener('mouseover', () => {
         square.classList.add("hover");
     })
 });
+}
+
+createGrid(16);
+
+
 
